f6aba6c72de1fe899277620167fd50e753380b7d
luckloro <145134541+luckloro@users.noreply.github.com>
2025-04-25 08:46:39 -0300

    O começo de tudo

--- 2025 04 24/Teste.py
+++ 2025 04 24/Teste.py
@@ -1,1 +1,7 @@
-print('Olá Mundo!')
+print(type('Olá mundo'))
+int (Number inteiro)
+float (Number Decimal)
+str (Text)
+# - Transformar a linha de código não funcional
+% (Resto da divisão)
+print(10 % 4) = 2
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Codes.of.Lucas
 My name is lucas, I'm starting in language of programming.
+<br>
 Meu nome é lucas, estou iniciando em linguagem de programação
