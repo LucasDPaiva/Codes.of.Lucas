90b4bff8352c8d12c1b72f20439e93210e1d773e
luckloro <lorinholukinhas@gmail.com>
2025-04-30 17:51:02 -0300

    Venda

--- 2025 04 30/Venda.py
+++ 2025 04 30/Venda.py
@@ -1,1 +1,6 @@
-valor_venda = float
+valorvenda = float(input('Digite o valor da venda (R$):'))
+percentual = float(input('Informe a comissão (%)......:'))
+
+comissão = valorvenda * percentual / 100
+
+print(f'O valor da comissão é R$ {comissão:.2f}')
