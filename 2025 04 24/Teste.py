d12111f91ae25e43c68eb15ac6a5a59bee5d650c
luckloro <145134541+luckloro@users.noreply.github.com>
2025-04-30 07:48:02 -0300

    Aula

--- 2025 04 24/Teste.py
+++ 2025 04 24/Teste.py
@@ -1,7 +1,7 @@
 print(type('Olá mundo'))
-int (Number Inteiro)
-float (Number Decimal)
-str (Text)
+int  ( Number Inteiro )
+float ( Number Decimal )
+str ( Text )
 # - Transformar a linha de código não funcional
 % (Resto da divisão)
 print(10 % 4) = 2
